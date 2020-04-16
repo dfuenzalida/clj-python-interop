9e067f46105e8a5f18f84f5fa18917604d864e6f
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2020-04-15 22:07:31 -0700

    Basic example

--- src/interop/core.clj
+++ src/interop/core.clj
@@ -1,7 +1,27 @@
 (ns interop.core
-  (:gen-class))
+  (:require [libpython-clj.require :refer [require-python]]
+            [libpython-clj.python :as py :refer [py. py.. py.-]]))
+;; See
+;; https://github.com/gigasquid/libpython-clj-examples/blob/master/src/gigasquid/_configure.clj
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(require-python '[builtins :as python])
+(require-python '[base64 :as pybase64])
+
+(defn encode-python [s]
+  (-> (python/bytearray s "ascii")
+      pybase64/b64encode
+      (py. decode "utf-8")))
+
+(defn decode-java [s]
+  (-> (.decode (java.util.Base64/getDecoder) s)
+      (String. "UTF-8")))
+
+;; Try these in the REPL:
+;; (encode-python "it works!")
+;; (decode-java "aXQgd29ya3Mh")
+
+(defn -main [& _args]
+  (py/initialize! :python-executable "env/bin/python3.6")
+  (-> "IT WORKS!" encode-python decode-java println)
+  (shutdown-agents))
+
