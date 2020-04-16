ba22f9298f7322b53b4b2e119428dbc7cf5f22d3
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2020-04-15 23:20:56 -0700

    Updated basic example

--- src/interop/core.clj
+++ src/interop/core.clj
@@ -8,9 +8,10 @@
 (require-python '[base64 :as pybase64])
 
 (defn encode-python [s]
-  (-> (python/bytearray s "ascii")
-      pybase64/b64encode
-      (py. decode "utf-8")))
+  (let [bytes   (python/bytearray s "ascii")  ;; bytes = bytearray(s, "ascii")
+        encoded (pybase64/b64encode bytes)    ;; encoded = base64.b64encode(bytes)
+        b64enc  (py. encoded decode "utf-8")] ;; b64enc = encoded.decode("utf-8")
+    b64enc))
 
 (defn decode-java [s]
   (-> (.decode (java.util.Base64/getDecoder) s)
@@ -20,8 +21,15 @@
 ;; (encode-python "it works!")
 ;; (decode-java "aXQgd29ya3Mh")
 
+;; See https://python-packaging.readthedocs.io/en/latest/minimal.html
+;; >>> import funniest
+;; >>> funniest.joke()
+
+;; (require-python '[funniest :as funniest])
+;; (funniest/joke)
+
 (defn -main [& _args]
-  (py/initialize! :python-executable "env/bin/python3.6")
+  (py/initialize! :python-executable "./env/bin/python3.6" :library-path "./env/bin/lib")
   (-> "IT WORKS!" encode-python decode-java println)
   (shutdown-agents))
 
